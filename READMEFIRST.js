57c4db0139cbbaa061bcebc189e1b809781b49ea
24LP3548 24LP3548 <24LP3548@G304-CC0228.local>
2024-12-16 11:52:14 +0800

    first react

---
+++ READMEFIRST.js
@@ -0,0 +1,8 @@
+// npm init
+// faker.js install
+// react router dom install
+// react scripts install
+
+// start => react-sctripts start bolgoh
+
+//gertee huulahdaa hamgiin ehend npm i ashiglah
---
+++ src/Home.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+const App = () => {
+  return <div>app added</div>;
+};
+export default App;
